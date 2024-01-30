2b5c0518325f0cae97daeac7e876754e61a5f3d2
Jesús <jesusmnc@hotmail.es>
2024-01-30 12:37:07 +0100

    ACTUALIZAR AVG

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Average finder v0.1");
         double avg = findAverage(args);
-        System.out.println("The average is " + avg);
+        System.out.println("The AVG is " + avg);
     }
 
     private static double findAverage(String[] input) {
